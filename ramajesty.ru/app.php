f6a82bb19aa457771483b2247dc56080f4d4e80a
Алексей Скляров <sufir@lightsoft.ru>
2016-06-05 19:09:56 +0300

    rep fix

--- ramajesty.ru/app.php
+++
@@ -1,9 +0,0 @@
-<?php
-require __DIR__.'/vendor/autoload.php';
-
-use App\RandomRowCommand;
-use Symfony\Component\Console\Application;
-
-$application = new Application();
-$application->add(new RandomRowCommand());
-$application->run();
