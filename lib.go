e6dbe69a7c770f7c808ee93853fe915b23627bd5
Алексей Скляров <asklyarov@ubic.tech>
2023-01-13 15:28:46 +0300

    cleanup: remove debug

--- lib.go
+++ lib.go
@@ -28,7 +28,7 @@ func NewLogExtended() *LogExtended {
 func (this *LogExtended) println(srcLogLvl LogLevel, prefix string, msg string) {
 	// игнорируем сообщения, если уровень логгера меньше scrLogLvl
 	if this.logLevel >= srcLogLvl {
-		this.Logger.Println(prefix + " " + msg + "[ " + strconv.Itoa(int(this.logLevel)) + " <= " + strconv.Itoa(int(srcLogLvl)) + " ]")
+		this.Logger.Println(prefix + " " + msg)
 	}
 }
 
